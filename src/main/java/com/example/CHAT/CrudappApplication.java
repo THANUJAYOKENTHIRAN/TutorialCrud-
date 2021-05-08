c2c3c64f0ec831a8090d0d0ab12dd6cfbc285317
THANUJAYOKENTHIRAN <77487073+THANUJAYOKENTHIRAN@users.noreply.github.com>
2021-05-08 20:19:35 +0530

    Update and rename CentappApplication.java to CrudappApplication.java

--- src/main/java/com/example/CHAT/CrudappApplication.java
+++ src/main/java/com/example/CHAT/CrudappApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CentappApplication {
+public class CrudappApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CentappApplication.class, args);
